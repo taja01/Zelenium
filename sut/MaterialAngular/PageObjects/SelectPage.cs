543cbc5f537d957e16c1486ca6e962c9937f7e45
taja01 <x-tamas.jarvas@wizzair.com>
2024-05-16 21:21:39 +0200

    adding select element test

---
+++ sut/MaterialAngular/PageObjects/SelectPage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenQA.Selenium;
+using Zelenium.Core.Model;
+using Zelenium.Core.WebDriver.Types;
+
+namespace MaterialAngular.PageObjects
+{
+    public class SelectPage : AbstractLoadableContainer
+    {
+        public SelectPage(ILogger logger, IWebDriver webDriver) : base(logger, webDriver, By.CssSelector(".docs-component-sidenav-content"), "https://material.angular.io/components/select/overview")
+        {
+        }
+
+        public SelectElement NativeSelect => Find<SelectElement>(By.CssSelector("#select-overview select"));
+
+        public override ValidationResult IsLoaded()
+        {
+            if (!this.Displayed)
+            {
+                return new ValidationResult { Passed = false, Message = $"Page load failed \n{this.Path}" };
+            }
+
+            return new ValidationResult { Passed = true, Message = "Ok" };
+        }
+    }
+}
---
+++ test/Zelenium.Core.IntegrationTests/WebElementTests/SelectElementTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using MaterialAngular.PageObjects;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Serilog;
+
+namespace Zelenium.Core.IntegrationTests.WebElementTests
+{
+    internal class SelectElementTests : BaseTest
+    {
+        private SelectPage page;
+        private ILogger<SelectElementTests> logger;
+
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            this.loggerFactory = new LoggerFactory().AddSerilog();
+            this.logger = this.loggerFactory.CreateLogger<SelectElementTests>();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.page = new SelectPage(this.logger, this.driver);
+            this.page.Load();
+            Assert.That(this.page.IsLoaded().Passed, Is.True);
+        }
+
+        [Test]
+        public void ReadOptionTest()
+        {
+            var options = this.page.NativeSelect.GetAllOptions();
+            Assert.That(options, Does.ContainKey("saab"));
+        }
+
+        [Test]
+        public void SetByIndex()
+        {
+            var options = this.page.NativeSelect.GetAllOptions();
+
+            this.page.NativeSelect.SetByIndex(3);
+
+            Assert.That(options.ElementAt(3).Value, Is.EqualTo(this.page.NativeSelect.SelectedText));
+        }
+
+        [Test]
+        public void SetByValue()
+        {
+            var options = this.page.NativeSelect.GetAllOptions();
+
+            this.page.NativeSelect.SetByValue("mercedes");
+
+            Assert.That(options["mercedes"], Is.EqualTo(this.page.NativeSelect.SelectedText));
+        }
+    }
+}
