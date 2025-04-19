21405628f8c54708528c15759df41601375bcf15
taja01 <jarvas.tamas@gmail.com>
2025-04-19 22:18:29 +0200

    refact radiobutton tests

--- test/Zelenium.Core.IntegrationTests/WebElementTests/RadioButtonTests.cs
+++ test/Zelenium.Core.IntegrationTests/WebElementTests/RadioButtonTests.cs
@@ -4,6 +4,10 @@ namespace Zelenium.Core.IntegrationTests.WebElementTests
 {
     public class RadioButtonTests : BaseTest
     {
+        private WebDriver.Types.InputField Radio1 => this.mainPage.RadioButtonsSection.Radio1;
+        private WebDriver.Types.InputField Radio2 => this.mainPage.RadioButtonsSection.Radio2;
+        private WebDriver.Types.InputField Radio3 => this.mainPage.RadioButtonsSection.Radio3;
+
         [SetUp]
         public void SetUp()
         {
@@ -13,9 +17,10 @@ namespace Zelenium.Core.IntegrationTests.WebElementTests
         [Test]
         public void DefaultTest()
         {
-            Assert.That(this.mainPage.RadioButtonsSection.Radio1.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio2.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio3.Selected, Is.False);
+
+            Assert.That(this.Radio1.Selected, Is.False);
+            Assert.That(this.Radio2.Selected, Is.False);
+            Assert.That(this.Radio3.Selected, Is.False);
         }
 
         [Test]
@@ -23,53 +28,53 @@ namespace Zelenium.Core.IntegrationTests.WebElementTests
         {
             this.mainPage.RadioButtonsSection.Radio1.Click();
 
-            Assert.That(this.mainPage.RadioButtonsSection.Radio1.Selected, Is.True);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio2.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio3.Selected, Is.False);
+            Assert.That(this.Radio1.Selected, Is.True);
+            Assert.That(this.Radio2.Selected, Is.False);
+            Assert.That(this.Radio3.Selected, Is.False);
         }
 
         [Test]
         public void Radio2Test()
         {
-            this.mainPage.RadioButtonsSection.Radio2.Click();
+            this.Radio2.Click();
 
-            Assert.That(this.mainPage.RadioButtonsSection.Radio1.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio2.Selected, Is.True);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio3.Selected, Is.False);
+            Assert.That(this.Radio1.Selected, Is.False);
+            Assert.That(this.Radio2.Selected, Is.True);
+            Assert.That(this.Radio3.Selected, Is.False);
         }
 
         [Test]
         public void Radio3Test()
         {
-            this.mainPage.RadioButtonsSection.Radio3.Click();
+            this.Radio3.Click();
 
-            Assert.That(this.mainPage.RadioButtonsSection.Radio1.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio2.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio3.Selected, Is.True);
+            Assert.That(this.Radio1.Selected, Is.False);
+            Assert.That(this.Radio2.Selected, Is.False);
+            Assert.That(this.Radio3.Selected, Is.True);
         }
 
         [Test]
         public void MixedTest_1()
         {
-            this.mainPage.RadioButtonsSection.Radio1.Click();
-            this.mainPage.RadioButtonsSection.Radio3.Click();
+            this.Radio1.Click();
+            this.Radio3.Click();
 
-            Assert.That(this.mainPage.RadioButtonsSection.Radio1.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio2.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio3.Selected, Is.True);
+            Assert.That(this.Radio1.Selected, Is.False);
+            Assert.That(this.Radio2.Selected, Is.False);
+            Assert.That(this.Radio3.Selected, Is.True);
         }
 
         [Test]
         public void MixedTest_2()
         {
-            this.mainPage.RadioButtonsSection.Radio1.Click();
-            this.mainPage.RadioButtonsSection.Radio2.Click();
-            this.mainPage.RadioButtonsSection.Radio3.Click();
-            this.mainPage.RadioButtonsSection.Radio2.Click();
+            this.Radio1.Click();
+            this.Radio2.Click();
+            this.Radio3.Click();
+            this.Radio2.Click();
 
-            Assert.That(this.mainPage.RadioButtonsSection.Radio1.Selected, Is.False);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio2.Selected, Is.True);
-            Assert.That(this.mainPage.RadioButtonsSection.Radio3.Selected, Is.False);
+            Assert.That(this.Radio1.Selected, Is.False);
+            Assert.That(this.Radio2.Selected, Is.True);
+            Assert.That(this.Radio3.Selected, Is.False);
         }
     }
 }
